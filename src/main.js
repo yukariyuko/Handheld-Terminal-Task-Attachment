5cbb2edc287c08f2ba9ac3796169f8baa2924b77
ZIYAN137 <zi_yan137@outlook.com>
2025-07-01 21:37:03 +0800

    refactor: 移除 npm 中的 easy player

--- src/main.js
+++ src/main.js
@@ -7,16 +7,10 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-// 引入EasyPlayer组件
-import EasyPlayer from '@easydarwin/easyplayer'
-
 // 引入mock数据
 import '../mock/initMock.js'
 
 const app = createApp(App)
-
-// 全局注册EasyPlayer组件
-app.component('EasyPlayer', EasyPlayer)
 
 app.use(ElementPlus)
 app.use(router)
